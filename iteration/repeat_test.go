3800eeb9fcb548147c38464c133f07ad6c8a440b
ncortim <ncortim@proton.me>
2024-06-05 12:00:12 +0200

    iteration exercise with benchmark

---
+++ iteration/repeat_test.go
@@ -0,0 +1,19 @@
+package iteration
+
+import "testing"
+
+// Let's write a test for a function that repeats a character 5 times
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%q' but got '%q'", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
