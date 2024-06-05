04729a6eb6126c21733df9dfb989909ba7c232a5
ncortim <ncortim@proton.me>
2024-06-05 12:00:12 +0200

    add number of iterations in caller, adapt benchmark, and create and exmaple

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, number int) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < number; i++ {
 		repeated += character
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,10 +1,13 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // Let's write a test for a function that repeats a character 5 times
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -14,6 +17,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", b.N)
+		// Repeat("a")
 	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("x", 5)
+	fmt.Println(repeat)
 }
